fe9e0775dd34dc7a59655ab987c5eb5b1e84738e
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-02-21 06:43:20 +0000

    finished

--- crowdsourcing.cpp
+++ crowdsourcing.cpp
@@ -11,6 +11,67 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
+int totalDuration(std::vector<int> songDuration)
+{
+    int Counter = 0;
+    int length = songDuration.size();
+    int totalDurations = 0;
+
+    while (Counter < length)
+    {
+        totalDurations = totalDurations + songDuration[Counter];
+        Counter = Counter + 1;
+    }
+    return totalDurations;
+}
+
+double averageDuration (std::vector<int> songDuration)
+{
+    return (double) totalDuration(songDuration)/length(songDuration);
+}
+
+
+void printlongestDuration (std::vector<std::string> songNames, std::vector<std::string> songArtist, std::vector<int>songDuration)
+{
+    int indexMax = 0;
+    int maxDuration = songDuration[0];
+    int curr_index = 0;
+
+    while (curr_index < length(songNames))
+    {
+        if (songDuration[curr_index] > maxDuration)
+        {
+            indexMax = curr_index;   
+            maxDuration = songDuration[curr_index];    
+        }
+        curr_index = curr_index + 1;
+    }
+
+    std::cout << "Longest song: " << songNames[indexMax] << " by " << songArtist[indexMax] << " for " << songDuration[indexMax] << " seconds\n";
+}
+
+
+void printshortestDuration (std::vector<std::string> songNames, std::vector<std::string> songArtist, std::vector<int>songDuration)
+{
+    int indexMin = 0;
+    int minDuration = songDuration[0];
+    int curr_index = 0;
+
+    while (curr_index < length(songNames))
+    {
+        if (songDuration[curr_index] < minDuration)
+        {
+            indexMin = curr_index;   
+            minDuration = songDuration[curr_index];    
+        }
+        curr_index = curr_index + 1;
+    }
+
+    std::cout << "Shortest song: " << songNames[indexMin] << " by " << songArtist[indexMin] << " for " << songDuration[indexMin] << " seconds\n";
+}
+
+
+
 int main()
 {
     
@@ -43,23 +104,15 @@ int main()
 
     }
 
-    
 
-    std::vector<int> totalDuration(std::vector<int> songDuration);
-    {
-        int Counter = 0;
-        int length = songDuration.size();
-        int totalDurations = 0;
 
-        while (Counter < length)
-        {
-            totalDurations = totalDurations + songDuration[Counter];
-            Counter = Counter + 1;
-        }
-        return totalDurations;
-    }
+   
 
-    std::cout << "The total duration is " << totalDuration << "secs" << std::endl;
 
+
+    std::cout << "The total duration: " << totalDuration (songDuration) << std::endl;
+    std::cout << "Avg Duration: " << averageDuration(songDuration) << std::endl;
+    printlongestDuration(songNames, songArtist, songDuration);
+    printshortestDuration(songNames, songArtist, songDuration);
     return 0;
 }
